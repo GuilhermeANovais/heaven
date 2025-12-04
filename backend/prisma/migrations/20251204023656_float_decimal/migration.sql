96cde44dd5d6069547c5fba2d7f1577cfe1b6cd5
Guilherme Novais <jose.guilherme.a.novais@gmail.com>
2025-12-03 23:39:31 -0300

    mudando de float para decimal no prisma para melhor precisão em calculos financeiros

---
+++ backend/prisma/migrations/20251204023656_float_decimal/migration.sql
@@ -0,0 +1,28 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `amount` on the `Expense` table. The data in that column could be lost. The data in that column will be cast from `DoublePrecision` to `Decimal(65,30)`.
+  - You are about to alter the column `totalRevenue` on the `MonthlyReport` table. The data in that column could be lost. The data in that column will be cast from `DoublePrecision` to `Decimal(65,30)`.
+  - You are about to alter the column `totalExpenses` on the `MonthlyReport` table. The data in that column could be lost. The data in that column will be cast from `DoublePrecision` to `Decimal(65,30)`.
+  - You are about to alter the column `netProfit` on the `MonthlyReport` table. The data in that column could be lost. The data in that column will be cast from `DoublePrecision` to `Decimal(65,30)`.
+  - You are about to alter the column `total` on the `Order` table. The data in that column could be lost. The data in that column will be cast from `DoublePrecision` to `Decimal(65,30)`.
+  - You are about to alter the column `price` on the `OrderItem` table. The data in that column could be lost. The data in that column will be cast from `DoublePrecision` to `Decimal(65,30)`.
+  - You are about to alter the column `price` on the `Product` table. The data in that column could be lost. The data in that column will be cast from `DoublePrecision` to `Decimal(65,30)`.
+
+*/
+-- AlterTable
+ALTER TABLE "Expense" ALTER COLUMN "amount" SET DATA TYPE DECIMAL(65,30);
+
+-- AlterTable
+ALTER TABLE "MonthlyReport" ALTER COLUMN "totalRevenue" SET DATA TYPE DECIMAL(65,30),
+ALTER COLUMN "totalExpenses" SET DATA TYPE DECIMAL(65,30),
+ALTER COLUMN "netProfit" SET DATA TYPE DECIMAL(65,30);
+
+-- AlterTable
+ALTER TABLE "Order" ALTER COLUMN "total" SET DATA TYPE DECIMAL(65,30);
+
+-- AlterTable
+ALTER TABLE "OrderItem" ALTER COLUMN "price" SET DATA TYPE DECIMAL(65,30);
+
+-- AlterTable
+ALTER TABLE "Product" ALTER COLUMN "price" SET DATA TYPE DECIMAL(65,30);
